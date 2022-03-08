ef9bbe4eac64fe52dfa4655e1331fccf5d58fd32
michaelgbenle <84715968+michaelgbenle@users.noreply.github.com>
2022-03-08 16:13:47 +0100

    work in progress

---
+++ go.mod
@@ -0,0 +1,3 @@
+module week-4-michaelgbenle
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "week-4-michaelgbenle/cmd"
+
+func main() {
+	cmd.Execute()
+}
